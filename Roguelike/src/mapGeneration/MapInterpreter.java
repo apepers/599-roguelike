b084fd1a8dcdd81e536a3f3f73d10df4eccc1ddd
kevinta893 <kevinta893@gmail.com>
2014-03-30 15:10:21 -0600

    Updating progress on Interpreter
    RIght now it needs to assign images to each tile.

--- Roguelike/src/mapGeneration/MapInterpreter.java
+++ Roguelike/src/mapGeneration/MapInterpreter.java
@@ -1,5 +1,7 @@
 package mapGeneration;
 
+import java.awt.Rectangle;
+
 import game.Map;
 import graphics.ImageRegistry;
 import entities.TileFactory;
@@ -15,13 +17,21 @@ public class MapInterpreter {
 
 	public static Map interpretMap(MapGenerator map, ImageRegistry[] registries){
 
-		Map newMap = new Map(map.getWidth(), map.getHeight());
+		if(registries.length < 1){
+			throw new IllegalArgumentException("Cannot interpret map with "+ registries.length + " registries.");
+		}
 		
-		//for each tile in the map, convert to an entity tile.
+		
+		Map newMap = new Map(map.getWidth(), map.getHeight());
+
+		//for each tile in the map, convert to an entity tile. Images not yet added.
 		for(int i= 0; i< map.getWidth(); i++){
 			for(int j =0; j < map.getHeight(); j++){
+
 				MapTile tile = map.getTile(i, j);
 				Tile newTile = null;
+
+				//go through each case
 				if (tile == MapTile.BLANK){
 					newTile = TileFactory.makeBlank();
 				}
@@ -33,7 +43,7 @@ public class MapInterpreter {
 						(tile == MapTile.PLAYER_SPAWN)){
 					//any type of floor
 					newTile = TileFactory.makeFloor();
-					
+
 				}
 				else if((tile == MapTile.WALL_H) || 
 						(tile == MapTile.WALL_V) || 
@@ -44,28 +54,69 @@ public class MapInterpreter {
 					//any type of wall
 					newTile = TileFactory.makeWall();
 				}
-				else if(tile == MapTile.DOOR){}
-				
-				
+				else if(tile == MapTile.DOOR){
+					//TODO Doors are passable for now.
+					newTile = TileFactory.makeFloor();
+				}
+
+
 				newMap.setTile(i, j, newTile);
 			}
 		}
-		for (MapTile tile : map){
-			if (tile == MapTile.BLANK){
-				
+
+
+		//for each room, decorate with the styles
+		for (Rectangle room : map.getRooms()){
+			ImageRegistry skin = registries[0];
+			
+			//paint each room with the appropriate tile
+			for(int i= room.x; i< room.x + room.width; i++){
+				for(int j =room.y; j < room.y + room.height; j++){
+					MapTile tile = map.getTile(i, j);
+					if (tile == MapTile.BLANK){}
+					else if(tile == MapTile.SPACE){}
+					else if(tile == MapTile.CORRIDOR_FLOOR){}
+					else if(tile == MapTile.WALL_H){}
+					else if(tile == MapTile.WALL_V){} 	
+					else if(tile == MapTile.WALL_TL_CORNER){} 
+					else if(tile == MapTile.WALL_TR_CORNER){} 
+					else if(tile == MapTile.WALL_BL_CORNER){} 
+					else if(tile == MapTile.WALL_BR_CORNER){}			
+					else if(tile == MapTile.ROOM_FLOOR){} 
+					else if(tile == MapTile.PLAYER_SPAWN){} 
+					else if(tile == MapTile.DOOR){}
+				}
 			}
-			else if(tile == MapTile.SPACE){}
-			else if(tile == MapTile.CORRIDOR_FLOOR){}
-			else if(tile == MapTile.WALL_H){}
-			else if(tile == MapTile.WALL_V){} 									//horizonal/vertical walls.
-			else if(tile == MapTile.WALL_TL_CORNER){} 
-			else if(tile == MapTile.WALL_TR_CORNER){} 
-			else if(tile == MapTile.WALL_BL_CORNER){} 
-			else if(tile == MapTile.WALL_BR_CORNER){}			//wall corners
-			else if(tile == MapTile.ROOM_FLOOR){} 
-			else if(tile == MapTile.PLAYER_SPAWN){} 
-			else if(tile == MapTile.DOOR){}
+			
+			
+			
 		}
+		
+
+
 		return null;
 	}
 }
+
+
+
+/* Template for many functions here. update with new MapTile members please.
+
+  for(int i= 0; i< map.getWidth(); i++){
+			for(int j =0; j < map.getHeight(); j++){
+				MapTile tile = map.getTile(i, j);
+				if (tile == MapTile.BLANK){}
+				else if(tile == MapTile.SPACE){}
+				else if(tile == MapTile.CORRIDOR_FLOOR){}
+				else if(tile == MapTile.WALL_H){}
+				else if(tile == MapTile.WALL_V){} 	
+				else if(tile == MapTile.WALL_TL_CORNER){} 
+				else if(tile == MapTile.WALL_TR_CORNER){} 
+				else if(tile == MapTile.WALL_BL_CORNER){} 
+				else if(tile == MapTile.WALL_BR_CORNER){}			
+				else if(tile == MapTile.ROOM_FLOOR){} 
+				else if(tile == MapTile.PLAYER_SPAWN){} 
+				else if(tile == MapTile.DOOR){}
+			}
+		}
+ */
