40c33441681e0ce31f2a69cf2e493ba64460a532
NolanHanna <nolan_rh@hotmail.com>
2014-03-31 22:03:44 -0600

    Removed the annoying .ser file
    It was quite annoying that every time you went to sync after running the
    program, you'd have to deal with the stupid .ser file that I was
    creating while duplicating items.  So I fixed the code so that it will
    no longer be there.

--- Roguelike/src/serialization/ItemDuplicator.java
+++ Roguelike/src/serialization/ItemDuplicator.java
@@ -1,5 +1,6 @@
 package serialization;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -10,19 +11,24 @@ public class ItemDuplicator {
 	public Object duplicate(Object object) {
 		try
 	      {
-	         FileOutputStream fileOut = new FileOutputStream("copy.ser");
-	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(object);
-	         out.close();
-	         fileOut.close();
-	         
-	         FileInputStream fileIn = new FileInputStream("copy.ser");
-	         ObjectInputStream in = new ObjectInputStream(fileIn);
-	         Object newObject = in.readObject();
-	         in.close();
-	         fileIn.close();
-	         
-	         return newObject;
+			
+			File f = new File("copy.ser");
+			
+			FileOutputStream fileOut = new FileOutputStream("copy.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(object);
+			out.close();
+			fileOut.close();
+			 
+			FileInputStream fileIn = new FileInputStream("copy.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			Object newObject = in.readObject();
+			in.close();
+			fileIn.close();
+			
+			System.out.println(f.delete());
+ 
+ return newObject;
 	      }catch(Exception e)
 	      {
 	          e.printStackTrace();
