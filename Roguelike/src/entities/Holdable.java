b19e79d04258712c11475fde885146b0c0af0379
Alexei Pepers <pikalex88@gmail.com>
2014-04-10 02:46:19 -0600

    Non-abstract holdable
    Made holdable non-abstract so that we can start adding misc items.

--- Roguelike/src/entities/Holdable.java
+++ Roguelike/src/entities/Holdable.java
@@ -5,11 +5,51 @@
 package entities;
 
 @SuppressWarnings("serial")
-public abstract class Holdable extends Entity {
+public class Holdable extends Entity {
 	private char id = 'a';
 	private int weight;
 	private int cost = 0;
 	private boolean stackable = false;
+	
+	public Holdable() {
+		
+	}
+	
+	public static String[] csvHeaders() {
+		String[] headers = {"Name", "Cost", "Weight", "Special"};
+		return headers;
+	}
+	
+	// Messy file parser just for demonstration, should be replaced by a way to read
+	// a comma-delimited list much more smoothly
+	public static Holdable createHoldableFromReader(String itemString) {
+		String[] values = itemString.split(",");
+		Holdable item = new Holdable();
+		try {
+			item.setName(values[0]);
+			item.setCost(Integer.parseInt(values[1]));
+			item.setWeight(Integer.parseInt(values[2]));
+			
+			if (values.length == 4 && values[3] != "") {
+				String[] specials = values[3].split(" ");
+				item = Holdable.applySpecialTraits(item, specials);
+			}
+		} catch (Exception e) {
+			System.out.println("Error reading misc object");
+			if (item.getName() != null)
+				System.out.println(item.getName() + " has some incorrect parameter.");
+			return null;
+		}
+		return item;
+	}
+	
+	// Apply the decorators as matched with the strings
+	public static Holdable applySpecialTraits(Holdable item, String[] traits) {
+		for (String trait : traits) {
+			
+		}
+		return item;
+	}
 	
 	public char getID() {
 		return id;
