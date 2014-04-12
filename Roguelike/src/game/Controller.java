0e4118abcd1226c6feb7ccc9cecaf419d4b5c663
kevinta893 <kevinta893@gmail.com>
2014-04-11 22:26:42 -0600

    Final game text.

--- Roguelike/src/game/Controller.java
+++ Roguelike/src/game/Controller.java
@@ -874,5 +874,6 @@ public class Controller {
 	 */
 	public void winGame(){
 		messenger.showTextDialog(GameText.getText("Win"), "You've won!");
+		messenger.showTextDialog("Your final Score is: " + player.getScore(), "Final Score");
 	}
 }
